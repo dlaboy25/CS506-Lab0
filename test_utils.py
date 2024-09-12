8b3b07038a9c1ae3ca5aa52532a4abfdcc439290
Derek Laboy <dlaboy@bu.edu>
2024-09-12 14:42:57 -0400

    created test and utils

--- test_utils.py
+++ test_utils.py
@@ -14,18 +14,21 @@ def test_dot_product():
     
 def test_cosine_similarity():
     ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([4, 5, 6])
+
+    result = cosine_similarity(vector1, vector2)
     
-    result = ### YOUR CODE HERE
-    
-    expected_result = ### YOUR CODE HERE
+    expected_result = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
-    ### YOUR CODE HERE
-    
-    result = ### YOUR CODE HERE
-    
-    expected_index = ### YOUR CODE HERE
-    
+    data = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    query = np.array([4, 5])
+
+    result = nearest_neighbor(data, query)
+
+    expected_index = 1 
+
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
